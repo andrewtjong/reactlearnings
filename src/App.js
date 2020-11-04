f6a7860db6263b2cdf75208ecba4c99ca96c1c16
andrewtjong <andrewttj@yahoo.com>
2020-11-04 21:50:28 +0100

    React Router, Switch, Route & Link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react"
 import Header from "./components/Header"
-import Footer from "./components/Footer"
 import Home from "./pages/Home"
-import Profile from "./pages/profile/Profile"
-import Info from "./pages/profile/Info"
-import Settings from "./pages/profile/Settings"
+import ServicesList from "./pages/services/ServicesList"
+import ServiceDetail from "./pages/services/ServiceDetail"
 
 import {Switch, Route} from "react-router-dom"
 
@@ -14,13 +12,13 @@ function App() {
             <Header />
             
             <Switch>
-                <Route exact path="/"><Home /></Route>
-                <Route exact path="/profile"><Profile/></Route>
-                <Route path="/profile/info"><Info /></Route>
-                <Route path="/profile/settings"><Settings /></Route>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/services" >
+                    <ServicesList />
+                </Route>
             </Switch>
-            
-            <Footer />
         </div>
     )
 }
